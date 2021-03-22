f3ffeaa80ed299491606d6a4a3494645098e9005
DemoniCross <63837016+DemoniCross@users.noreply.github.com>
2021-03-22 12:04:35 +0300

    Create prog.c

---
+++ lab07/prog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "prog.h"
+int main(){
+/* local var declarations */
+fsp = process_file("wcs.txt");
+/* printf the returned stats */
+lines = get_lines("wcs.txt");
+return 0;
+}
+/* define the functions
+you can write function stubs
+during development */
